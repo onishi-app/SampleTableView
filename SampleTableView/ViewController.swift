2579fcd03c4e8e90af07fd1c226ccfc22959ef53
Onishi Ryosuke <onishi.appcreate@gmail.com>
2021-01-01 15:43:21 +0900

    投稿時間表示
    現在の時間から投稿された時間を引き何分前に投稿されたかを表示する。
    また、名前も追加しました。

--- SampleTableView/ViewController.swift
+++ SampleTableView/ViewController.swift
@@ -9,17 +9,52 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    let item = ["Cell 1Cell 1Cell 1Cell 1Cell 1Cell 1Cell 1Cell 1Cell 1Cell 1Cell 1Cell 1Cell 1Cell 1Cell 1Cell 1Cell 1Cell 1Cell 1Cell 1Cell 1Cell 1Cell 1Cell 1Cell 1Cell 1Cell 1", "Cell 2", "Cell 3", "Cell 4", "Cell 5"]
+    var item = [
+        ["content": "2020年\nが\n終わります。\n今年も\n良い\n一年\nでした。", "date": Date().timeIntervalSince1970 - 313123123, "name": "斎藤"],
+        ["content": "良いお年をお過ごしください。", "date": Date().timeIntervalSince1970 - 123212312, "name": "本田"],
+        ["content": "新年明けましておめでとうございます。", "date": Date().timeIntervalSince1970 - 20000000, "name": "鈴木"],
+        ["content": "お年玉を1万円あげます。", "date": Date().timeIntervalSince1970 - 2323232, "name": "川崎"],
+        ["content": "2021年はもっと勉強を頑張ります。", "date": Date().timeIntervalSince1970 - 13213, "name": "三菱"],
+        ["content": "今年は本厄なので気を引き締めます。", "date": Date().timeIntervalSince1970 - 3333, "name": "豊田"],
+        ["content": "今年もよろしくお願いします。", "date": Date().timeIntervalSince1970 - 10, "name": "武田"]
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         let tableView = UITableView(frame: self.view.bounds, style: .plain)
+        
         tableView.delegate = self
         tableView.dataSource = self
         
         tableView.register(UINib(nibName: "CustomTableViewCell", bundle: nil), forCellReuseIdentifier: "customCell")
-        
+
         view.addSubview(tableView)
+    }
+    
+    func timeCheck(now: TimeInterval, past: TimeInterval) -> String {
+        let timeDiff = Int(now - past)
+        
+        let year = timeDiff / 31104000
+        let month = timeDiff / 2592000
+        let day = timeDiff / 86400
+        let hour = timeDiff / 3600
+        let minute = timeDiff / 60
+        let second = timeDiff
+        
+        if year != 0 {
+            return "\(year)年前"
+        } else if month != 0 {
+            return "\(month)ヶ月前"
+        } else if day != 0 {
+            return "\(day)日前"
+        } else if hour != 0 {
+            return "\(hour)時間前"
+        } else if minute != 0 {
+            return "\(minute)分前"
+        } else {
+            return "\(second)秒前"
+        }
+        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -28,7 +63,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "customCell", for: indexPath) as! CustomTableViewCell
-        cell.content.text = self.item[indexPath.row]
+        let now = Date().timeIntervalSince1970
+        item = item.reversed()
+        
+        let past = item[indexPath.row]["date"] as! TimeInterval
+        cell.time.text = timeCheck(now: now, past: past)
+        cell.content.text = (item[indexPath.row]["content"] as! String)
+        cell.name.text = (item[indexPath.row]["name"] as! String)
+        
         return cell
     }
 }
