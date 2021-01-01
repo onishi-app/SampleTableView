9d1b729e8c863607be4009cfb30fa47d8653a5f5
Onishi Ryosuke <onishi.appcreate@gmail.com>
2021-01-01 11:29:09 +0900

    tableView、tableViewCell基盤作成
    tableViewの最低限の実装とcustomCellのUI実装を行いました。

---
+++ SampleTableView/CustomTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTableViewCell.swift
+//  SampleTableView
+//
+//  Created by ryosuke on 2021/01/01.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+
+    }
+    
+}
--- SampleTableView/ViewController.swift
+++ SampleTableView/ViewController.swift
@@ -7,13 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    let item = ["Cell 1", "Cell 2", "Cell 3", "Cell 4", "Cell 5"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let tableView = UITableView(frame: self.view.bounds, style: .plain)
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        view.addSubview(tableView)
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return item.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        cell.textLabel?.text = item[indexPath.row]
+        return cell
+    }
 }
 
